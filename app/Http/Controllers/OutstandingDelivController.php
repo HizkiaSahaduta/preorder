8e213ade853eb77b0d187b67ac7a19261f9702c9
HizkiaSahaduta <hizkiasahaduta@gmail.com>
2021-04-21 13:29:02 +0700

    api_outstanding

--- app/Http/Controllers/OutstandingDelivController.php
+++ app/Http/Controllers/OutstandingDelivController.php
@@ -24,90 +24,116 @@ class OutstandingDelivController extends Controller
     public function getOutstandingDeliv(Request $request){
 
         $groupid = Session::get('GROUPID');
-        $txtCustID = Session::get('CUSTID');
 
         if ($groupid == "CUSTOMER") {
 
-            $data = DB::connection("sqlsrv2")
-                    ->select(DB::raw("select ltrim(rtrim(a.order_id)) as order_id, ltrim(rtrim(a.cust_name)) as cust_name,
-                            FORMAT(a.dt_order, 'dd MMM yyyy') as dt_order,
-                            a.leat_time, a.descr, 
-                            CONVERT(varchar(12), b.thick) as thick, 
-                            CAST(b.width as float) as width, 
-                            b.grade_id, b.coat_mass, 
-                            CONVERT(varchar(12), a.wgt_ord) as wgt_ord, 
-                            CONVERT(varchar(12), a.wgt_deliv) as wgt_deliv,
-                            CASE
-                                WHEN (a.wgt_ord - a.wgt_deliv) < 0 THEN  '('+CONVERT(varchar(12),((a.wgt_ord - a.wgt_deliv) * -1))+')'
-                                ELSE CONVERT(varchar(12),(a.wgt_ord - a.wgt_deliv))
-                            END as outstd,
-                            CONVERT(varchar(12), a.wgt_lpm) as wgt_lpm, 
-                            CASE
-                                WHEN ((a.wgt_ord - a.wgt_deliv) - a.wgt_lpm) < 0 THEN '('+CONVERT(varchar(12),(((a.wgt_ord - a.wgt_deliv) - a.wgt_lpm) * -1))+')'
-                                ELSE CONVERT(varchar(12),((a.wgt_ord - a.wgt_deliv) - a.wgt_lpm))
-                            END as remain
-                            from tracking_order a
-                            inner join prod_spec b on a.prod_code = b.prod_code
-                            where cust_id = '$txtCustID' and order_id <> ''"));
+            $custid = Session::get('CUSTID');
 
+            $token = Session::get('token');
+
+            $url = "https://svr01.kencana.org/service/api/getOutstandingDeliv?custid=".$custid;
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_TIMEOUT => 30000,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array(
+                    // Set Here Your Requesred Headers
+                    'Content-Type: application/json',
+                    "Authorization: Bearer $token",
+                ),
+            ));
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+
+            if ($err) {
+                return view('layouts.OutstandingDeliv')->with('error','OutstandingDeliv#:' . $err);
+            }
+
+            else {
+
+                $data = json_decode($response);
+            }
         }
 
         if ($groupid == "SALES") {
 
             $salesid = Session::get('SALESID');
 
-            $data = DB::connection("sqlsrv2")
-                    ->select(DB::raw("select ltrim(rtrim(a.order_id)) as order_id, ltrim(rtrim(a.cust_name)) as cust_name,
-                    FORMAT(a.dt_order, 'dd MMM yyyy') as dt_order,
-                    a.leat_time, a.descr, 
-                    CONVERT(varchar(12), b.thick) as thick, 
-                    CAST(b.width as float) as width, 
-                    b.grade_id, b.coat_mass, 
-                    CONVERT(varchar(12), a.wgt_ord) as wgt_ord, 
-                    CONVERT(varchar(12), a.wgt_deliv) as wgt_deliv,
-                    CASE
-                        WHEN (a.wgt_ord - a.wgt_deliv) < 0 THEN  '('+CONVERT(varchar(12),((a.wgt_ord - a.wgt_deliv) * -1))+')'
-                        ELSE CONVERT(varchar(12),(a.wgt_ord - a.wgt_deliv))
-                    END as outstd,
-                    CONVERT(varchar(12), a.wgt_lpm) as wgt_lpm, 
-                    CASE
-                        WHEN ((a.wgt_ord - a.wgt_deliv) - a.wgt_lpm) < 0 THEN '('+CONVERT(varchar(12),(((a.wgt_ord - a.wgt_deliv) - a.wgt_lpm) * -1))+')'
-                        ELSE CONVERT(varchar(12),((a.wgt_ord - a.wgt_deliv) - a.wgt_lpm))
-                    END as remain
-                    from tracking_order a
-                    inner join prod_spec b on a.prod_code = b.prod_code
-                    inner join order_book_hdr c on a.book_id = c.book_id
-                    where c.salesman_id = '$salesid' and a.order_id <> ''"));
+            $token = Session::get('token');
+
+            $url = "https://svr01.kencana.org/service/api/getOutstandingDeliv?salesid=".$salesid;
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_TIMEOUT => 30000,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array(
+                    // Set Here Your Requesred Headers
+                    'Content-Type: application/json',
+                    "Authorization: Bearer $token",
+                ),
+            ));
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+
+            if ($err) {
+                return view('layouts.OutstandingDeliv')->with('error','OutstandingDeliv#:' . $err);
+            }
+
+            else {
+
+                $data = json_decode($response);
+            }
 
         }
 
         if ($groupid == 'DEVELOPMENT' || $groupid == 'MARKETING MANAGEMENT') {
 
-            $data = DB::connection("sqlsrv2")
-            ->select(DB::raw("select ltrim(rtrim(a.order_id)) as order_id, ltrim(rtrim(a.cust_name)) as cust_name,
-                    FORMAT(a.dt_order, 'dd MMM yyyy') as dt_order,
-                    a.leat_time, a.descr, 
-                    CONVERT(varchar(12), b.thick) as thick, 
-                    CAST(b.width as float) as width, 
-                    b.grade_id, b.coat_mass, 
-                    CONVERT(varchar(12), a.wgt_ord) as wgt_ord, 
-                    CONVERT(varchar(12), a.wgt_deliv) as wgt_deliv,
-                    CASE
-                        WHEN (a.wgt_ord - a.wgt_deliv) < 0 THEN  '('+CONVERT(varchar(12),((a.wgt_ord - a.wgt_deliv) * -1))+')'
-                        ELSE CONVERT(varchar(12),(a.wgt_ord - a.wgt_deliv))
-                    END as outstd,
-                    CONVERT(varchar(12), a.wgt_lpm) as wgt_lpm, 
-                    CASE
-                        WHEN ((a.wgt_ord - a.wgt_deliv) - a.wgt_lpm) < 0 THEN '('+CONVERT(varchar(12),(((a.wgt_ord - a.wgt_deliv) - a.wgt_lpm) * -1))+')'
-                        ELSE CONVERT(varchar(12),((a.wgt_ord - a.wgt_deliv) - a.wgt_lpm))
-                    END as remain
-                    from tracking_order a
-                    inner join prod_spec b on a.prod_code = b.prod_code
-                    where order_id <> ''"));
+            $token = Session::get('token');
 
+            $url = "https://svr01.kencana.org/service/api/getOutstandingDeliv?privilege=all";
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_TIMEOUT => 30000,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array(
+                    // Set Here Your Requesred Headers
+                    'Content-Type: application/json',
+                    "Authorization: Bearer $token",
+                ),
+            ));
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+
+            if ($err) {
+                return view('layouts.OutstandingDeliv')->with('error','OutstandingDeliv#:' . $err);
+            }
+
+            else {
+
+                $data = json_decode($response);
+            }
         }
-
-       
 
         return \DataTables::of($data)
         ->editColumn('outstd', function ($data) { 
